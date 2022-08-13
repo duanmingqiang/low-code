556bd50748b7b688d4b508edc5db5526c5d3bb2b
duanmingqiang <964178976@qq.com>
2022-08-13 22:09:57 +0800

    fix:组件id异常

--- src/views/design/design-editor/DesignPanel.vue
+++ src/views/design/design-editor/DesignPanel.vue
@@ -33,6 +33,14 @@ const dragLeave = function (event) {
     event.preventDefault();
     event.stopPropagation();
 }
+// 生成组件唯一ID 利用时间戳+随机字母生成，永远不会重复
+function createId () {
+	let abc=['a','b','c','d','e','f','g','h','i','g','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+	let [max,min]=[Math.floor(Math.random()*(10-7+1)+1),Math.floor(Math.random()*(17-10+1)+17)];
+	abc=abc.sort(()=>0.4-Math.random()).slice(max,min).slice(0,8).join("");
+	var a=new Date().getTime()+abc;
+	return a
+}
 // 若拖放元素到了目标元素中（在目标元素中松开鼠标），就会触发drop事件而不会触发dragleave事件
 const drop = function (event) {
     const currentComponent = JSON.parse(JSON.stringify(store.state.currentDragComponent))
@@ -40,23 +48,23 @@ const drop = function (event) {
     const props = currentComponent.props || {}
     // 解析模块的slots配置
     // const slots = {}
-    // currentComponent.slots && Object.keys(currentComponent.slots).forEach(item => {
-    //   if (currentComponent.slots[item].slotName) {
-    //     slots[item] = currentComponent.slots[item].slotName
-    //   }
-    // })
     const slots = currentComponent.slots || {}
+    currentComponent.slots && Object.keys(currentComponent.slots).forEach(item => {
+      currentComponent.slots[item].slotList && currentComponent.slots[item].slotList.forEach(slot => {
+        const componentId = createId()
+        slot.props.id = componentId
+        console.log('slot', slot)
+      })
+    })
     // 生成唯一ID
-    const componentId = (new Date()).getTime().toString()
+    const componentId = createId()
     editorData.value = [...editorData.value, {
         'component-name': currentComponent['component-name'],
         props: {
-          // ...options,
           id: componentId,
           ...props
         },
         slots
-        // slots: currentComponent.slots || {}
     }]  
     event.preventDefault();
     event.stopPropagation();
